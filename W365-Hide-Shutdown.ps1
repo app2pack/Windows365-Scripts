1a9e11e664d130798eae6d8ea97fd3dbf8f079c6
Vigneshwaran <vigneshwaran4264@gmail.com>
2024-10-28 23:27:58 +0530

    Create W365-Hide-Shutdown.ps1

---
+++ W365-Hide-Shutdown.ps1
@@ -0,0 +1,25 @@
+# Define the path to the registry subkey
+$subkeyPath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\Hideshutdown"
+
+# Define the name of the DWORD value to check and set
+$dwordName = "value"
+
+# Check if the registry subkey exists
+if (-not (Test-Path $subkeyPath)) {
+    # Create the registry subkey if it doesn't exist
+    New-Item -Path $subkeyPath -Force
+    Write-Output "Registry subkey '$subkeyPath' created."
+}
+
+# Check if the DWORD value exists
+$dwordExists = Get-ItemProperty -Path $subkeyPath -Name $dwordName -ErrorAction SilentlyContinue
+
+if ($null -ne $dwordExists) {
+    # Set the DWORD value to 1
+    Set-ItemProperty -Path $subkeyPath -Name $dwordName -Value 1 -Type DWord
+    Write-Output "Registry DWORD value '$dwordName' set to 1 in '$subkeyPath'."
+} else {
+    # Create the DWORD value and set it to 0
+    New-ItemProperty -Path $subkeyPath -Name $dwordName -Value 1 -PropertyType DWord
+    Write-Output "Registry DWORD value '$dwordName' created and set to 1 in '$subkeyPath'."
+}
